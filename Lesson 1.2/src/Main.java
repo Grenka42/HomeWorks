f8f71541d0021f19b14a0a66e60cc35dcbda9469
Alexey <122717143+Grenka42@users.noreply.github.com>
2023-03-22 05:24:40 +0300

    Home Work 2
    Использование \n (перенос на новую строку)

---
+++ Lesson 1.2/src/Main.java
@@ -0,0 +1,12 @@
+//Home Work 2
+public class Main {
+	public static void main(String[] args) {
+		System.out.println("**************************");
+		System.out.println("****       Визитка    ****");
+		System.out.println("****  Name: you name  ****");
+		System.out.println("**** Skype: you skype ****");
+		System.out.println("**************************");
+		
+		System.out.println("\n**************************\n****       Визитка    ****\n****  Name: you name  ****\n**** Skype: you skype ****\n**************************");
+	}
+}
