ad58dd38243efb121b632cde110f1ceb8c010545
Alexey <122717143+Grenka42@users.noreply.github.com>
2023-03-22 05:25:08 +0300

    Home Work 3
    Отступ \t (красная строка)

---
+++ Lesson 1.3/src/Main.java
@@ -0,0 +1,12 @@
+//Home Work 3
+public class Main {
+	public static void main(String[] args) {
+		System.out.print("\t**************************\n");
+		System.out.print("\t****       Визитка    ****\n");
+		System.out.print("\t****  Name: you name  ****\n");
+		System.out.print("\t**** Skype: you skype ****\n");
+		System.out.print("\t**************************\n");
+		
+		System.out.print("\n**************************\n****       Визитка    ****\n****  Name: you name  ****\n**** Skype: you skype ****\n**************************");
+	}
+}
